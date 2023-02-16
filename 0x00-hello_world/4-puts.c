c65afc3a686126bc69711efbda062d78451b6939
oluwatosin <oluwatosinajisola2@gmail.com>
2023-02-16 12:08:02 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
+
 
 /**
- * main - Entry point
- *
- * Return: Always 0(Success) 
- */ 
-int main(void) 
- {
+ *  main - A program that prints a line with puts fucntion
+ *  Description: prints "\"Programming is like building a multilingual puzzle"
+ *  Return: 0
+ */
+
+int main(void)
+{
         puts("\"Programming is like building a multilingual puzzle");
-        return (0); 
- } 
-
-
+        return (0);
+}
